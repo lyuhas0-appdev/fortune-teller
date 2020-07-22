e10c225150d1101707e7e73d4b2950bfab361a7c
lyuhas0 <lyuhas0@chicagobooth.edu>
2020-07-22 00:11:11 +0000

    done with picses

--- app/controllers/water_controller.rb
+++ app/controllers/water_controller.rb
@@ -38,6 +38,6 @@ class WaterController < ApplicationController
       @array_of_numbers.push(another_number)
     end
     
-    render({ :template => "aqua_views/pisces.html.erb" })
+    render({ :template => "aqua/pisces.html.erb" })
   end
 end
